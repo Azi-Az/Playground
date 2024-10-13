5dbe22b74c6ca7ae80a459bd90a0c38a32227e91
Azi <natalianowacka2002@gmail.com>
2024-10-13 08:25:38 +0200

    conditions

--- Main.java
+++ Main.java
@@ -1,1 +1,12 @@
-init123
+public class Main {
+    public static void main(String() args) {
+        int number = 5;
+        int secondNumber = 8;
+
+        if (number>secondNumber) {
+            System.out.println("Your number is bigger than: "+secondNumber);
+        } else {
+            System.out.println("Your number is smaller than: "+secondNumber)
+        }
+    }
+}
