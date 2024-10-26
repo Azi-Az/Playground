3b7f71d6b7c43c27fd3eafee9ab627d523c848b7
Student <dembek.patrick@gmail.com>
2024-10-26 11:52:24 +0200

    notatki

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,115 @@
+package org.example;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Program do konwersji stopni C na F");
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
+
+//double temp;
+//
+//boolean done = false;
+//
+//        while (!done) {
+//
+//        try {
+//Scanner input = new Scanner(System.in);
+//                System.out.print("Podaj temperaturę w stopniach C: ");
+//temp = input.nextDouble();
+//                input.close();
+//
+//double secondTemp = 1.8 * temp + 32.0;
+//
+//                System.out.println("Temperatura w stopniach F to: " + secondTemp);
+//
+//done = true;
+//
+//        } catch (InputMismatchException e) {
+//        System.out.println("Proszę podać poprawną liczbę.");
+//            }
+//                    }
+
+
+
+
+
+
+//        boolean done = false;
+//
+//        while (!done) {
+//            try {
+//                int number;
+//                Scanner input = new Scanner(System.in);
+//                System.out.println("Podaj liczbę: ");
+//                number = input.nextInt();
+//                input.close();
+//                for (int i = 0; i < number; i++) {
+//                    for (int j = 1; j <= number; j++) {
+//                        if (j < number) {
+//                            System.out.print(j + ", ");
+//                        } else {
+//                            System.out.print(j + " ");
+//                        }
+//                    }
+//                    System.out.println();
+//                }
+//                done = true;
+//            } catch (InputMismatchException e) {
+//                System.out.println("Proszę podać liczbę całkowitą");
+//            }
+//        }
+
+
+
+
+//        int number = 10;
+//
+//        for (int i = 0; i < 10; i++) {
+//            for (int j = 1; j <= 10; j++)
+//            System.out.print(i + ", ");
+
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Podaj liczbę: ");
+//        number = input.nextInt();
+//        input.close();
+//
+//        int number;
+//
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Podaj liczbę: ");
+//
+//        try {
+//            number = input.nextInt();
+//            System.out.println("Podana liczba to: " + number);
+//
+//        } catch (InputMismatchException e) {
+//            System.out.println("Proszę podać liczbę całkowitą");
+//        }
+//        input.close();
+
+//        for (int i = 0; i < number; i++) {
+//            for (int j = 1; j <= number; j++) {
+//                if (j < number) {
+//                    System.out.print(j + ", ");
+//                } else {
+//                    System.out.print(j + " ");
+//                }
+//            }
+//            System.out.println();
+
